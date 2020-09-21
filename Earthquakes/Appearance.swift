ea22bc109cd7b7437d4418fc37c16a2f6b07e0c7
Richard Kelly <rk@codeden.dev>
2020-09-21 18:49:58 +0100

    navBar color change
    Still need to correct map locations to determine new location of earthquakes.

--- Earthquakes/Appearance.swift
+++ Earthquakes/Appearance.swift
@@ -9,29 +9,55 @@
 import UIKit
 
 class Appearance: NSObject {
-    
-      static func setUpApperance() {
-            guard let _ = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate else {
+    static func setUpApperance() {
+        if #available(iOS 13.0, *) {
+            guard let _ = UIApplication.shared.delegate as? AppDelegate else {
                 return
             }
-            self.setupNavigationBar()
+        } else {
+            // Fallback on earlier versions
         }
-        
-        static func setupNavigationBar() {
-            let appearance = UINavigationBarAppearance()
-            appearance.configureWithTransparentBackground()
-            appearance.backgroundColor = UIColor.Font.navRed
-            UINavigationBar.appearance().barTintColor = UIColor.Font.darkGreen
-            UINavigationBar.appearance().titleTextAttributes =  [NSAttributedString.Key.foregroundColor:UIColor.white]
-            UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
-            UINavigationBar.appearance().isTranslucent = false
-            
-            UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.orange]
-            
-    //        appearance.shadowColor = nil
-            UINavigationBar.appearance().tintColor = UIColor.Font.darkGreen
-            UINavigationBar.appearance().standardAppearance = appearance
-            UINavigationBar.appearance().scrollEdgeAppearance = appearance
-        }
-}
 
+        self.setupNavigationBar()
+       // self.setupTabBar()
+      //  self.setupTableView()
+    }
+    /// This method set the colour scheme of the nav bar
+    static func setupNavigationBar() {
+        
+        if #available(iOS 13.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.backgroundColor = .red
+            appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+
+            UINavigationBar.appearance().tintColor = .white
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().compactAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        } else {
+            UINavigationBar.appearance().tintColor = .white
+            UINavigationBar.appearance().barTintColor = .red
+            UINavigationBar.appearance().isTranslucent = false
+        }
+    }
+    
+    /// This method set the colour scheme of the tab bar
+//    static func setupTabBar() {
+//        let appearance = UITabBar.appearance()
+//        appearance.backgroundColor = UIColor.Font.greendd
+//        appearance.tintColor = UIColor.TabBar.selectedTab
+//        appearance.unselectedItemTintColor = UIColor.TabBar.unSelectedTab
+//        appearance.backgroundImage = UIImage()
+//
+//        let attributes = [NSAttributedString.Key.font: UIFont.Regular.size0]
+//        UITabBarItem.appearance().setTitleTextAttributes(attributes, for: .normal)
+//        UITabBarItem.appearance().setTitleTextAttributes(attributes, for: .selected)
+//    }
+//
+//    static func setupTableView() {
+//        let appearance = UITableView.appearance()
+//        //appearance.separatorInset = .zero
+//        appearance.tableFooterView = UIView()
+//    }
+}
